e4c3a63a9b840695e0906241278dbfae51f54637
EuChat <grkchataika@gmail.com>
2024-04-23 13:58:31 +0200

    Prep program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random magicNumber = new Random();
+        int answer = magicNumber.Next(1, 100);
+        int guessCount = 0;
+        int possible = 0;
+        bool loop = true;
+        do
+        {
+            do
+            {
+                Console.Write("What is your guess? ");
+                string guess = Console.ReadLine();
+                possible = int.Parse(guess);
+                guessCount ++;
+                
+                
+                if (possible < answer)
+                {
+                    Console.WriteLine("Higher\n");
+                }
+                else if (possible > answer)
+                {
+                    Console.WriteLine("Lower\n");
+                }
+                else 
+                {
+                    Console.WriteLine($"You guessed it!\n It took you {guessCount} guesses.\n");
+                    Console.Write("Would you like to play again? (lower case) ");
+                    string cont = Console.ReadLine();
+                    if (cont == "yes" )
+                    {
+                        loop = true;
+                    }
+                    else
+                    {
+                        loop = false;
+                    }
+                }
+            }
+            while (answer != possible);
+        }
+        while(loop);
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int square = SquareNumber(favNumber);
+        DisplayResult(name, square);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favourite number: ");
+        string stringNumber = Console.ReadLine();
+        int number = int.Parse(stringNumber);
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int square = number*number;
+        return square;
+    }
+    static void DisplayResult(string name, int SquareNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {SquareNumber}");
     }
 }
