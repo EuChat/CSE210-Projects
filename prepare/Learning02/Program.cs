276d6655f81e8e01adfb2794d160f2a393d6386b
EuChat <grkchataika@gmail.com>
2024-05-01 09:50:57 +0200

    Resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -17,7 +17,7 @@ class Program
         job2._endYear = 2023;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Chatayika Garikai";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
